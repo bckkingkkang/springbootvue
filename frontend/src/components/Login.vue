f2f3e8ca89cd68cca6496155d1895064f631d0fe
gh.ko <gh.ko@renocomms.com>
2024-03-18 15:01:59 +0900

    vuex store.js 추가

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -2,21 +2,21 @@
   <div>
     <form action="" @submit.prevent>
       <div>
-        <label for="userId">아이디 : </label>
-        <input type="text" id="userId" v-model="userid">
+        <label for="userid">아이디 : </label>
+        <input type="text" id="userid" v-model="userid">
       </div>
       <div>
         <label for="password">비밀번호 : </label>
         <input type="text" id="password" v-model="password">
       </div>
-      <button v-on:click="login">로그인</button>
+      <button v-on:click="isLogin">로그인</button>
       <button><router-link to="/Signup">회원가입</router-link></button>
     </form>
 
   </div>
 </template>
 
-<script setup>
+<script>
 import axios from 'axios';
 import router from "@/router/index.js"
 import { ref } from 'vue';
@@ -24,13 +24,28 @@ import { ref } from 'vue';
 const userid = ref('');
 const password = ref('');
 
-function login() {
-  const response = axios.post(`/api/member/loginCheck_ajax`, {
-    userid : userid.value,
-    password : password.value
-  }).then(response => {
-        console.log(response)
-      });
+export default {
+  data() {
+    return {
+      userid : '',
+      password : ''
+    }
+  },
+  methods : {
+    isLogin() {
+      if(this.userid == '') {
+        alert('ID를 입력하세요');
+        return;
+      }
+      if(this.password == '') {
+        alert("PASSWORD를 입력하세요");
+        return;
+      }
+
+      alert('로그인되었습니다.');
+    },
+
+  }
 }
 
 
---
+++ frontend/src/vuex/store.js
@@ -0,0 +1,60 @@
+import {createStore} from "vuex";
+import getters from "./getters";
+import mutations from "./mutations";
+
+/*
+* Store : 어플리케이션 상태를 보유하고 있는 컨테이너
+*
+*/
+
+export default createStore({
+    state : {
+        /*
+        * state : 여러 컴포넌트 간에 공유되는 데이터를 의미
+        *   - 정의
+        *   new Vuex.Store({
+        *       state : {
+        *           message : 'hello Vue.js'
+        *   })
+        *   - 접근
+        *       <div>{{ $store.state.message }} </div>
+        */
+        user : null,
+        isLogin : false,
+    },
+    mutations,
+    /*
+        mutations : vuex에서 상태 값을 변경하는 유일한 방법으로 상태는 항상 뮤테이션으로 변경된다.
+            - 정의
+            new Vuex.Store({
+                mutations : {
+                    reverserMessage(state) {
+                        state.message.split('').reverse().join('');
+                    }
+                }
+            })
+            - 호출
+            new Vue({
+                methods : {
+                    reverseMsg() {
+                        this.$store.commit('reverseMessage');
+                    }
+                }
+            }
+    */
+
+    getters,
+    /*
+        getters : computed 속성과 같은 역할, 상태(state) 값이 변경되었을 때 변화에 따른 차이를 자동으로 반영하여 값을 계산해 줌
+            - 정의
+            new Vuex.Store({
+                getters : {
+                    reverserMessage(state) {
+                        return state.message.split('').reverse().join('');
+                    }
+                }
+            })
+            - 접근
+            <div>{{ $store.getters.reverseMessage }}</div>
+    */
+})
